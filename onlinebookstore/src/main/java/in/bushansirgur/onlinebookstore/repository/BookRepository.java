21e41da8db5ccc408235f6c68e4161ca28f83e7b
umeshjavadeveloper <40715026+umeshjavadeveloper@users.noreply.github.com>
2020-11-27 00:09:30 +0530

    Created angular project and completed the feature 1.

--- onlinebookstore/src/main/java/in/bushansirgur/onlinebookstore/repository/BookRepository.java
+++ onlinebookstore/src/main/java/in/bushansirgur/onlinebookstore/repository/BookRepository.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 import in.bushansirgur.onlinebookstore.entity.Book;
 
+@CrossOrigin("http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book, Long> {
 
 	/*
